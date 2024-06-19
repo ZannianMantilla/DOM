23731f1187cacfe5c1dd3f6e77812e22da5924f3
Zannian Mantilla Celis <zanniancelis@gmail.com>
2024-06-19 10:08:53 -0500

    Primero

---
+++ app.js
@@ -0,0 +1,11 @@
+// Objeto padre en el navegador
+let win = window;
+let dom = document;
+let head = dom.head;
+let body = dom.body;
+
+
+console.log(win)
+console.log(dom)
+console.log(head)
+console.log(body)
